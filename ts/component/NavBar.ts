7a08652f61b30c77e13019a15415e0c74fa18d00
Fousse Emelyne <emelyne.fousse8@etu.univ-lorraine.fr>
2026-06-03 15:04:07 +0200

    fix du bug de la nav bar

--- ts/component/NavBar.ts
+++ ts/component/NavBar.ts
@@ -1,12 +1,12 @@
-export class NavBar extends HTMLElement {
+class NavBar extends HTMLElement {
     connectedCallback(): void {
         this.innerHTML = `
         <nav class="main-nav">
             <ul>
-                <li><a href="index.html">Accueil</a></li>
-                <li><a href="velib.html">Carte Vélib</a></li> 
-                <li><a href="carte-restaurant.html">Carte Restaurant</a></li>
-                <li><a href="carte-incidents.html">Carte Incidents</a></li>
+                <li><a href="/index.html">Accueil</a></li>
+                <li><a href="/html/velib.html">Carte Vélib</a></li> 
+                <li><a href="/html/carte-restaurant.html">Carte Restaurant</a></li>
+                <li><a href="/html/carte-incidents.html">Carte Incidents</a></li>
             </ul>
         </nav>
         `;
