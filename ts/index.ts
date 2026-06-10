2fb91b9e3061cc32a65bb952078a8b96f27ada57
Hermann Taïno <06tainor@gmail.com>
2026-06-10 17:07:29 +0200

    Deplacement sidbar et rename des pages

--- ts/index.ts
+++ ts/index.ts
@@ -1,12 +1,3 @@
+// Chargement de la navbar dans la page d'acceuil
 import './component/NavBar';
 
-const toggle = document.getElementById("sidebar-toggle") as HTMLDivElement | null;
-const sidebar = document.getElementById("sidebar") as HTMLDivElement | null;
-
-if (toggle && sidebar) {
-    toggle.addEventListener("click", () => {
-        sidebar.classList.toggle("open");
-
-        toggle.textContent = sidebar.classList.contains("open") ? "❯" : "❮" ;
-    });
-}
