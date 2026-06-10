9a198d510a0a6c634944d47901573ccaed50c839
Hermann Taïno <06tainor@gmail.com>
2026-06-10 09:04:47 +0200

    Autofuocus sur la carte

--- ts/velomap.ts
+++ ts/velomap.ts
@@ -1,9 +1,13 @@
 import { getStationsInfo } from "./api_velib.mjs";
-import { VILLES } from "./config.mjs";
-import { addVelibStation, clearMap } from "./map.mjs";
+import { VILLES, VILLES_COORDONEES } from "./config.mjs";
+import { addVelibStation, centerMap, clearMap } from "./map.mjs";
 
 async function loadVille(ville: VILLES) {
     clearMap();
+
+    const [lat, lon] = VILLES_COORDONEES[ville];
+    centerMap(lat, lon);
+
     const response = await getStationsInfo(ville);
     const stations = response.data.stations;
     for (const station of stations) {
